61a8cc7d62166a5b69047843956ced0356a1dc89
Jose Mauricio Cabrera Elias <jmcabrerae@gmail.com>
2023-05-13 20:46:42 -0700

    more work on portfolio, added, a few folders updated html

---
+++ portfolio/src/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+  
+    return (
+      <footer>
+            <div className="col">
+              <div className="copyright py-4 text-center">
+                <div className="container">
+                  <small>
+                    Copyright  &copy; {"J.Cabeli LLC"}
+                  </small>
+                </div>
+              </div>
+            </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
